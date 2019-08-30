bebcfee2bcb368ba67fb76d0db2e715be6142d03
Toby <tdudleyoffice@gmail.com>
2019-08-30 17:47:46 -0500

    make blog detail pages appear on homepage

--- home/models.py
+++ home/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import (
-    FieldPanel,
-    MultiFieldPanel
-)
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.models import Page
+
+from blog.models import BlogDetailPage
 
 
 class HomePage(Page):
     """Home page model/ landing page"""
+
     template = "home/home_page.html"
     max_count = 1
     banner_title = models.CharField(max_length=20, blank=False, null=True)
@@ -23,9 +23,26 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel("banner_title"),
-            FieldPanel("banner_subtitle"),
-            ImageChooserPanel("banner_image")
-        ], heading="Banner Options")
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+            ],
+            heading="Banner Options",
+        )
     ]
+
+    def get_context(self, request, *args, **kwargs):
+        """Getting blog posts into context"""
+
+        context = super().get_context(request)
+
+        posts = (
+            BlogDetailPage.objects.live()
+            .public()
+            .order_by("-first_published_at")
+        )
+
+        context["posts"] = posts
+        return context
