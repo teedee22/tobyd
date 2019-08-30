095892f00dd83ec1ae30c019d9d8a424724cf0e0
Toby <tdudleyoffice@gmail.com>
2019-08-29 21:14:02 -0500

    install django-extensions, django-debug-toolbar; consolidate templates

--- tobyd/settings/dev.py
+++ tobyd/settings/dev.py
@@ -7,9 +7,20 @@ DEBUG = True
 SECRET_KEY = '+6mcv^w^_h)@($76i)s#4^ujl9na-i0ghx*&tp_+7!qk@f$mr4'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INSTALLED_APPS = INSTALLED_APPS + [
+    'django_extensions',
+    'debug_toolbar',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
 
 
 try:
