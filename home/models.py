9d84e86c724ca36d2349c4aa8e07862edcbd181b
Toby <tdudleyoffice@gmail.com>
2019-08-30 14:49:49 -0500

    add models and template for home page banner

--- home/models.py
+++ home/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel
+)
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+    max_count = 1
+    banner_title = models.CharField(max_length=20, blank=False, null=True)
+    banner_subtitle = models.CharField(max_length=200, blank=True, null=True)
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image")
+        ], heading="Banner Options")
+    ]
