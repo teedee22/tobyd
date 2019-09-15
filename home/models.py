19ea40c692df1bf657b76aa253a2cf4d7c6e25d7
Toby <tdudleyoffice@gmail.com>
2019-09-15 14:07:38 -0500

    add about and featured post to homepage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+)
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
 from blog.models import BlogDetailPage
@@ -22,6 +27,26 @@ class HomePage(Page):
         related_name="+",
     )
 
+    about_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    about_title = models.CharField(max_length=100, blank=True, null=True)
+    about_text = RichTextField(
+        features=["bold", "italic"], blank=True, null=True
+    )
+
+    featured_blog_post = models.ForeignKey(
+        "wagtailcore.page",
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+    )
+
     content_panels = Page.content_panels + [
         MultiFieldPanel(
             [
@@ -30,7 +55,16 @@ class HomePage(Page):
                 ImageChooserPanel("banner_image"),
             ],
             heading="Banner Options",
-        )
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("about_title"),
+                ImageChooserPanel("about_image"),
+                FieldPanel("about_text"),
+            ],
+            heading="About me box",
+        ),
+        PageChooserPanel("featured_blog_post"),
     ]
 
     def get_context(self, request, *args, **kwargs):
@@ -41,7 +75,7 @@ class HomePage(Page):
         posts = (
             BlogDetailPage.objects.live()
             .public()
-            .order_by("-first_published_at")
+            .order_by("-first_published_at")[:1]
         )
 
         context["posts"] = posts
