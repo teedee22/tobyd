f139b2e8d58d52bc367192e6b670b04de01e6167
Toby <tdudleyoffice@gmail.com>
2019-09-19 11:26:13 -0500

    add s3 caching

--- tobyd/settings/production.py
+++ tobyd/settings/production.py
@@ -43,7 +43,8 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
