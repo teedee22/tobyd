5c54a817608a3911dd44840be82d2bbea44f35c3
Toby <tdudleyoffice@gmail.com>
2019-09-25 15:13:31 -0500

    add stack images to about and contact page, add streamfields to contact

--- contact/models.py
+++ contact/models.py
@@ -6,15 +6,31 @@ from wagtail.admin.edit_handlers import (
     FieldRowPanel,
     InlinePanel,
     MultiFieldPanel,
+    StreamFieldPanel
 )
 
 from wagtailcaptcha.models import WagtailCaptchaEmailForm
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.forms.models import (
     AbstractEmailForm,
     AbstractFormField
 )
+from streamfs import blocks
+
+
+class StackImages(Orderable):
+    page = ParentalKey("contact.ContactPage", related_name="stack_images")
+    stack_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    stack_url = models.URLField(blank=True, null=True)
+    panels = [ImageChooserPanel("stack_image"), FieldPanel("stack_url")]
 
 
 class FormField(AbstractFormField):
@@ -37,10 +53,18 @@ class ContactPage(WagtailCaptchaEmailForm):
 
     intro = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
-
+    content = StreamField(
+        [
+            ("full_richtext", blocks.RichTextBlock()),
+            ("single_image_block", blocks.SingleImageBlock()),
+        ],
+        null=True,
+        blank=True,
+    )
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel('intro'),
         ImageChooserPanel("banner_image"),
+        StreamFieldPanel("content"),
         InlinePanel('form_fields', label="Form Fields"),
         FieldPanel('thank_you_text'),
         MultiFieldPanel([
@@ -50,4 +74,11 @@ class ContactPage(WagtailCaptchaEmailForm):
             ]),
             FieldPanel('subject'),
         ], heading="Email Settings"),
+        MultiFieldPanel(
+            [
+                InlinePanel(
+                    "stack_images", max_num=10, min_num=1, label="stack images"
+                )
+            ],
+            heading="Images of tech stack used",),
     ]
